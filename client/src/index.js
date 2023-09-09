1c75a10b8e04ba578bbd627085cddcf5e878ccbf
Harshzzz <harshmishra@Harshs-MacBook-Air.local>
2023-09-09 21:40:30 +0530

    some changes

--- client/src/index.js
+++ client/src/index.js
@@ -5,16 +5,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import { Firstpage } from './FirstPage';
-import { BrowserRouter } from 'react-router-dom';
+import {Firstpage} from "./FirstPage.js"
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+    <Firstpage />
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
